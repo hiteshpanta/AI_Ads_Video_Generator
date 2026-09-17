4a5716c5d0ab34ecfad2dfd35ff4d4d04c8b3bd0
__Hitesh__ <hiteshpant50@gmail.com>
2026-09-17 23:42:01 +0545

    changes

--- src/app/api/get-avatar-list/route.tsx
+++ src/app/api/get-avatar-list/route.tsx
@@ -12,7 +12,7 @@ export async function GET(req: any) {
     });
 
 
-      const result = await axios.get('https://api.heygen.com/v3/avatars',{
+      const result2 = await axios.get('https://api.heygen.com/v3/avatars',{
         headers: {
             	"x-api-key": `${process.env.HEYGEN_API_KEY}`
         }
